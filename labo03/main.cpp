389d797af4afeeb34806f5aceec92036b0f06fdf
user <user@user-PC>
2016-10-17 10:22:02 +0100

    base

--- labo03/main.cpp
+++ labo03/main.cpp
@@ -4,17 +4,36 @@
  *
  * Created on 17 octobre 2016, 10:01
  */
-
-#include <cstdlib>
-
+#include <string>
+#include <iostream>
+#include <sstream>
 using namespace std;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    cout<< "hello"<< endl;
 
+
+    string str; // une chaine de caractères
+
+    if (cin >> str) {
+        // atteint si la chaine a pu etre lue
+    }
+
+    if (str.empty()) {
+        // atteint si la chaine est vide
+    }
+
+    stringstream ss(str); // flux qui lit depuis la chaine str
+    // exactement comme cin lit depuis la console
+
+    int val; // un entier
+
+    if (ss >> val) {
+        // code atteint si l'entier val a pu etre lu depuis
+        // le flux ss.
+    }
     return 0;
 }
 
