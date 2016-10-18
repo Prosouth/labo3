cf44973cc67cab1b132ebf7e7c6451aca82740d4
Sebastien Saez <monsieursaez@gmail.com>
2016-10-19 00:05:04 +0200

    code refactoring

--- labo03/main.cpp
+++ labo03/main.cpp
@@ -43,9 +43,7 @@ int main()
          if(str[0] == '$')
          {
             cout << "Je t'ai reconnu frère" << endl;
-         }
-         
-         
+         }  
       }
 
       if (str.empty()) 
@@ -68,101 +66,40 @@ int main()
          switch(val)
          {
             case 1: 
-               cout << "Tu as fait 1" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 2: 
-               cout << "Tu as fait 2" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 3: 
-               cout << "Tu as fait 3" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 4: 
-               cout << "Tu as fait 4" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 5: 
-               cout << "Tu as fait 5" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 6: 
-               cout << "Tu as fait 6" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 7: 
-               cout << "Tu as fait 7" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 8: 
-               cout << "Tu as fait 8" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 9: 
-               cout << "Tu as fait 9" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 10: 
-               cout << "Tu as fait 10" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 11: 
-               cout << "Tu as fait 11" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 12: 
-               cout << "Tu as fait 12" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 13: 
-               cout << "Tu as fait 13" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 14: 
-               cout << "Tu as fait 14" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 15: 
-               cout << "Tu as fait 15" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 16: 
-               cout << "Tu as fait 16" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 17: 
-               cout << "Tu as fait 17" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 18: 
-               cout << "Tu as fait 18" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 19: 
-               cout << "Tu as fait 19" << endl;
-               score_actuel = score_actuel - val;
-               break;
             case 20: 
-               cout << "Tu as fait 20" << endl;
                score_actuel = score_actuel - val;
+               if(nombre_de_flechette_actuel >= 3)
+               {
+                  nombre_de_flechette_actuel = 1;
+               }
+               else
+               {
+                  nombre_de_flechette_actuel++;
+               }
                break;               
             default:
                cout << "Ta valeur n'a pas été trouvée" << endl;
          }
-      }
-      
- 
-     if(nombre_de_flechette_actuel == 3)
-     {
-        nombre_de_flechette_actuel == 1;
-     }
-     else
-     {
-        nombre_de_flechette_actuel++;
-     }
-   } while (score_actuel == 0);
-
+      }     
+   } while (score_actuel != 0);
 
 
    return EXIT_SUCCESS;
