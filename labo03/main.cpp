1af0d63821df4f7b6f7013148cbbc6c7d683d285
user <user@user-PC>
2016-10-27 19:18:59 +0100

    tout est bien reste les commentaire et lafffichage de score ..

--- labo03/main.cpp
+++ labo03/main.cpp
@@ -2,15 +2,15 @@
  -----------------------------------------------------------------------------------
  Laboratoire : Laboratoire 3 - Convertisseur avec les entiers
  Fichier     : main.cpp
- Auteur(s)   : Sébastien Saez et Aymen Lagha
+ Auteur(s)   : SÃ©bastien Saez et Aymen Lagha
  Date        : 17 octobre 2016
 
  But         : Le but de ce laboratoire est de mettre en oeuvre un programme
  *             qui compte les points pour un joueur au 501 double out, une
- *             variante des fléchettes.
+ *             variante des flÃ©chettes.
 
- Remarque(s) : Si l'utilisateur rentre des chiffres à virgule, on ne prend
- *             que la partie entière.
+ Remarque(s) : Si l'utilisateur rentre des chiffres Ã  virgule, on ne prend
+ *             que la partie entiÃ¨re.
 
  Compilateur : g++ (GCC) 6.2.1
  -----------------------------------------------------------------------------------
@@ -34,27 +34,29 @@ int main() {
 
     do
     {
-        string str; // une chaine de caractères
+        string str; // une chaine de caractÃ¨res
 
-        cout << "Score: " << score_actuel << "  - Jouez la flechette " 
-             << nombre_de_flechette_actuel << "/" << FLECHETTES_MAX << endl;
+        cout << "Score: " << score_actuel;
+        cout << "  - Jouez la flechette " << nombre_de_flechette_actuel;
+        cout << "/" << FLECHETTES_MAX << endl;
 
 
         if (cin >> str)
-        {
-            // atteint si la chaine a pu etre lue
-            //recuperation de l'indication s'il est double ou triple
-            char indice_de_coefficient = tolower(str[0]);
-            //convertion en code ascii
-            int val_premier_caractere = indice_de_coefficient;
-
-            if (val_premier_caractere == 100 || val_premier_caractere == 116)
+        {           
+            // atteint si la chaine a pu etre lue            
+            
+            
+            //convertion en code ascii          
+            int codeascii = str[0];
+            if(codeascii== 68 || codeascii == 84 ){
+                codeascii +=32;//convertion de caractere entre maj et minuscule
+            }
+           
+            if (codeascii == 100 || codeascii == 116 )
             {
                 //on ecrase la premier caractere par le caractere zero pour puis
                 //convertir apres avec la "stringstream"
                 str[0] = '0';
-
-
                 //recupere le reste de la chaine pour 
                 //elimine la valeur 25 de triple
                 string reste_de_chaine = "";
@@ -62,27 +64,20 @@ int main() {
                 reste_de_chaine += str[2];
 
 
-                if (indice_de_coefficient == 't' && !(reste_de_chaine == "25"))
+                if (codeascii == 116 && !(reste_de_chaine == "25"))
                 {
-
-                    coefficient = 3;
-                    //score_actuel = score_actuel - coefficient;
+                    coefficient = 3;                    
                 }
                 else
                 {
-
-                    if (indice_de_coefficient == 'd')
-                    {
-
+                    if (codeascii == 100)
+                    {                       
                         coefficient = 2;
-
                     }
                     else
                     {
-
                         str = "";
                     }
-
                 }
             }
         }
@@ -91,7 +86,7 @@ int main() {
         {
             // atteint si la chaine est vide
             //cout << "Entree non valide" << endl;
-            
+            break;
         }
 
         stringstream ss(str); // flux qui lit depuis la chaine str
@@ -106,42 +101,29 @@ int main() {
             // le flux ss.
             // ICI c'est seulement les valeurs simples
             unsigned int score_dun_coup = 0;
-
-            if (val >= 0 && val <= 20)
-            {
+            
+            if ((val >= 0 && val <= 20)||(val == 25)||(val == 25 && coefficient == 2))
+            {              
                 score_dun_coup = coefficient * val;
-
                 nombre_de_flechette_actuel++;
             }
             else
-            {
-                if ((val == 25) || (val == 25 && coefficient == 2))
-                {
-                    score_dun_coup = coefficient * val;
-                    nombre_de_flechette_actuel++;
-                }
-                else
-                {
+            {                
                     nombre_flechettes_total--;
-                    cout << "Entree non valide" << endl;
-                }
-
-            }
-
-
-            coefficient = 1; //remettre les coefficient à la valeur initiale
+                    cout << "Entree non valide" << endl;            
+            }          
 
             int resultat_soustraction = score_actuel - score_dun_coup;
-            if (resultat_soustraction == 0 || resultat_soustraction > 1)
-            {
+           
+            if ((resultat_soustraction == 0 && coefficient==2 )|| resultat_soustraction > 1)
+            {                
                 score_de_volee_courant += score_dun_coup;
-                score_actuel -= score_dun_coup;
+                score_actuel -= score_dun_coup;               
             }
             else
             {
                 if (score_dun_coup > score_actuel || resultat_soustraction < 2)
                 {
-
                     score_actuel += score_de_volee_courant;
                     score_de_volee_courant = 0;
                 }
@@ -153,8 +135,8 @@ int main() {
                 nombre_de_flechette_actuel = 1;
                 score_de_volee_courant = 0;
             }
-
         }
+         coefficient = 1; //remettre les coefficient a valeur initialle
         nombre_flechettes_total++;
     }
     while (score_actuel != 0);
@@ -163,3 +145,4 @@ int main() {
     cout << "Bravo!" << endl;
     return 0;
 }
+
