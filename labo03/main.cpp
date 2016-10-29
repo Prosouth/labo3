55e309f77cad9b23a5e3173b2ab76fe01d894d92
Sebastien Saez <monsieursaez@gmail.com>
2016-10-29 18:10:46 +0200

    fixed bug with score and few improvements

--- labo03/main.cpp
+++ labo03/main.cpp
@@ -1,8 +1,8 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : Laboratoire 3 - Convertisseur avec les entiers
- Fichier     : main.cpp
- Auteur(s)   : Sébastien Saez et Aymen Lagha
+ Laboratoire : Laboratoire 3 - 501 double out
+ Fichier     : Labo3_lagha_saez.cpp
+ Auteur(s)   : Aymen Lagha et Sébastien Saez 
  Date        : 17 octobre 2016
 
  But         : Le but de ce laboratoire est de mettre en oeuvre un programme
@@ -10,7 +10,7 @@
  *             variante des fléchettes.
 
  Remarque(s) : Si l'utilisateur rentre des chiffres à virgule, on ne prend
- *             que la partie entière.
+ *             que la partie entière. 
 
  Compilateur : g++ (GCC) 6.2.1
  -----------------------------------------------------------------------------------
@@ -24,13 +24,13 @@ using namespace std;
 const unsigned int SCORE_AU_DEBUT = 501, FLECHETTES_MAX = 3;
 
 // variables globales
-unsigned int score_actuel = SCORE_AU_DEBUT;
+int score_actuel = SCORE_AU_DEBUT;
 int nombre_flechettes_total = 0;
 
 int main() {
     unsigned int nombre_de_flechette_actuel = 1;
     unsigned int coefficient = 1;
-    unsigned int score_de_volee_courant = 0;
+    unsigned int score_de_volee_courante = 0;
 
     do
     {
@@ -64,7 +64,6 @@ int main() {
 
                 if (indice_de_coefficient == 't' && !(reste_de_chaine == "25"))
                 {
-
                     coefficient = 3;
                     //score_actuel = score_actuel - coefficient;
                 }
@@ -73,16 +72,12 @@ int main() {
 
                     if (indice_de_coefficient == 'd')
                     {
-
                         coefficient = 2;
-
                     }
                     else
                     {
-
                         str = "";
                     }
-
                 }
             }
         }
@@ -90,7 +85,7 @@ int main() {
         if (str.empty())
         {
             // atteint si la chaine est vide
-            //cout << "Entree non valide" << endl;
+           cout << "Entree non valide" << endl;
             
         }
 
@@ -104,7 +99,7 @@ int main() {
         {
             // code atteint si l'entier val a pu etre lu depuis
             // le flux ss.
-            // ICI c'est seulement les valeurs simples
+            // les valeurs simples sont traitées ici
             unsigned int score_dun_coup = 0;
 
             if (val >= 0 && val <= 20)
@@ -125,35 +120,32 @@ int main() {
                     nombre_flechettes_total--;
                     cout << "Entree non valide" << endl;
                 }
-
             }
 
-
-            coefficient = 1; //remettre les coefficient à la valeur initiale
+            coefficient = 1; // on remet les coefficients à la valeur initiale
 
             int resultat_soustraction = score_actuel - score_dun_coup;
             if (resultat_soustraction == 0 || resultat_soustraction > 1)
             {
-                score_de_volee_courant += score_dun_coup;
+                score_de_volee_courante += score_dun_coup;
                 score_actuel -= score_dun_coup;
             }
             else
             {
                 if (score_dun_coup > score_actuel || resultat_soustraction < 2)
                 {
-
-                    score_actuel += score_de_volee_courant;
-                    score_de_volee_courant = 0;
+                    //score_actuel += score_de_volee_courante;
+                    score_de_volee_courante = 0;
+                    nombre_de_flechette_actuel--;
                 }
-                nombre_de_flechette_actuel = 1;
+                //nombre_de_flechette_actuel = 1;
                 cout << "Bust" << endl;
             }
             if (nombre_de_flechette_actuel > 3)
             {
                 nombre_de_flechette_actuel = 1;
-                score_de_volee_courant = 0;
+                score_de_volee_courante = 0;
             }
-
         }
         nombre_flechettes_total++;
     }
@@ -161,5 +153,5 @@ int main() {
     cout << "Score: " << score_actuel << " en " << nombre_flechettes_total;
     cout << " flechettes" << endl;
     cout << "Bravo!" << endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
