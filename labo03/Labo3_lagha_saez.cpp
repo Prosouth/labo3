1b94a08f934ea7b655423125c58ed2ff88653c04
Sebastien Saez <monsieursaez@gmail.com>
2016-10-30 15:32:57 +0100

    formatting ftw

--- labo03/Labo3_lagha_saez.cpp
+++ labo03/Labo3_lagha_saez.cpp
@@ -26,7 +26,8 @@ const unsigned int SCORE_AU_DEBUT = 501,
 const char         LETTRE_D_MAJ = 'D',
                    LETTRE_D_MIN = 'd',
                    LETTRE_T_MAJ = 'T',
-                   LETTRE_T_MIN = 't';
+                   LETTRE_T_MIN = 't',
+                   CONVERSION_MINUSCULE = 32;
 
 int main()
 {
@@ -46,14 +47,17 @@ int main()
       cout << "/" << FLECHETTES_MAX << endl;
 
       // atteint si la chaîne a pu être lue            
-      if (cin >> str) {
+      if (cin >> str) 
+      {
          //conversion en code ASCII          
          char codeascii = str[0];
-         if (codeascii == LETTRE_D_MAJ || codeascii == LETTRE_T_MAJ) {
-            codeascii += 32; // conversion entre majuscule et minuscule
+         if (codeascii == LETTRE_D_MAJ || codeascii == LETTRE_T_MAJ) 
+         {
+            codeascii += CONVERSION_MINUSCULE; // conversion entre majuscule et minuscule
          }
 
-         if (codeascii == LETTRE_D_MIN || codeascii == LETTRE_T_MIN) {
+         if (codeascii == LETTRE_D_MIN || codeascii == LETTRE_T_MIN) 
+         {
             // on écrase le premier caractère par le caractère zéro afin
             // de le convertir par la suite avec "stringstream"
             str[0] = '0';
@@ -62,19 +66,26 @@ int main()
             reste_de_chaine += str[1];
             reste_de_chaine += str[2];
 
-            if (codeascii == LETTRE_T_MIN && !(reste_de_chaine == "25")) {
+            if (codeascii == LETTRE_T_MIN && !(reste_de_chaine == "25")) 
+            {
                coefficient = 3;
-            } else {
-               if (codeascii == LETTRE_D_MIN) {
+            } 
+            else 
+            {
+               if (codeascii == LETTRE_D_MIN) 
+               {
                   coefficient = 2;
-               } else {
+               } 
+               else 
+               {
                   str = "";
                }
             }
          }
       }
 
-      if (str.empty()) {
+      if (str.empty()) 
+      {
          // atteint si la chaine est vide
          cout << "Entree non valide" << endl;
       }
@@ -84,39 +95,49 @@ int main()
       int valeur_tir; // variable utilisée pour recevoir la valeur du tir
 
       //si l'entier valeur_tir a pu être lu depuis le flux ss
-      if (ss >> valeur_tir) {
+      if (ss >> valeur_tir) 
+      {
          unsigned int score_dun_coup = 0;
 
          // Traitement des valeurs simples uniquement
          if ((valeur_tir >= 0 && valeur_tir <= 20) || (valeur_tir == 25) ||
-                 (valeur_tir == 25 && coefficient == 2)) {
+             (valeur_tir == 25 && coefficient == 2)) 
+         {
             score_dun_coup = coefficient * valeur_tir;
             nombre_de_flechette_actuel++;
-         } else {
+         } 
+         else 
+         {
             nombre_flechettes_total--;
             cout << "Entree non valide" << endl;
          }
 
          int resultat_soustraction = score_actuel - score_dun_coup;
 
-         if ((resultat_soustraction == 0 && coefficient == 2) || resultat_soustraction > 1) {
+         if ((resultat_soustraction == 0 && coefficient == 2) || resultat_soustraction > 1) 
+         {
             score_de_volee_courante += score_dun_coup;
             score_actuel -= score_dun_coup;
-         } else {
-            if (score_dun_coup > score_actuel || resultat_soustraction < 2) {
+         } 
+         else 
+         {
+            if (score_dun_coup > score_actuel || resultat_soustraction < 2) 
+            {
                score_de_volee_courante = 0;
                nombre_de_flechette_actuel--;
             }
             cout << "Bust" << endl;
          }
-         if (nombre_de_flechette_actuel > 3) {
+         if (nombre_de_flechette_actuel > 3) 
+         {
             nombre_de_flechette_actuel = 1;
             score_de_volee_courante = 0;
          }
       }
       coefficient = 1; // on remet les coeff. à la valeur initiale après le traitement
       nombre_flechettes_total++;
-   } while (score_actuel != 0);
+   } 
+   while (score_actuel != 0);
 
    cout << "Score: " << score_actuel << " en " << nombre_flechettes_total;
    cout << " flechettes" << endl;
