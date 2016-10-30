af73ab5207b6df10ac1fab04bd7384b01774d51e
Sebastien Saez <monsieursaez@gmail.com>
2016-10-30 18:01:48 +0100

    final comments

--- labo03/Labo3_lagha_saez.cpp
+++ labo03/Labo3_lagha_saez.cpp
@@ -10,7 +10,10 @@
  *             variante des fléchettes.
  
  Remarque(s) : Si l'utilisateur rentre des chiffres à virgule, on ne prend
- *             que la partie entière. 
+ *             que la partie entière. On voulait implémenter un message d'erreur 
+ *             si on rentre "z30" par exemple mais la manière dont codecheck fait
+ *             les tests nous aurait fait échouer nos tests. Du coup, si on rentre
+ *             z30, on a aucun retour à l'utilisateur et rien ne change.
  
  Compilateur : g++ (GCC) 6.2.1
  -----------------------------------------------------------------------------------
@@ -48,20 +51,23 @@ int main()
       // atteint si la chaîne a pu être lue            
       if (cin >> str) 
       {
-         //conversion en code ASCII          
-         char codeascii = str[0];
-         if (codeascii == LETTRE_D_MAJ || codeascii == LETTRE_T_MAJ) 
+         // premier caractère de la string entrée          
+         char premier_caractere = str[0];
+         
+         // Si la première lettre correspond à D ou T
+         if (premier_caractere == LETTRE_D_MAJ || premier_caractere == LETTRE_T_MAJ) 
          {
-            codeascii += CONVERSION_MINUSCULE; // conversion entre majuscule et minuscule
+            premier_caractere += CONVERSION_MINUSCULE; // conversion entre majuscule et minuscule
          }
 
-         if (codeascii == LETTRE_D_MIN || codeascii == LETTRE_T_MIN) 
+         if (premier_caractere == LETTRE_D_MIN || premier_caractere == LETTRE_T_MIN) 
          {
             // on écrase le premier caractère par le caractère zéro afin
             // de le convertir par la suite avec "stringstream"
             str[0] = '0';
             
-            switch(codeascii)
+            // permet d'assigner les coefficients aux bonnes lettres
+            switch(premier_caractere)
             {
                case LETTRE_T_MIN:
                   coefficient = 3;
@@ -76,9 +82,9 @@ int main()
          }
       }
 
+      // atteint si la chaine est vide
       if (str.empty()) 
       {
-         // atteint si la chaine est vide
          cout << "Entree non valide" << endl;
       }
 
@@ -99,19 +105,20 @@ int main()
             nombre_de_flechette_actuel++;
             nombre_flechettes_total++;
          } 
-         else 
+         else // autrement l'entrée est invalide
          {
             cout << "Entree non valide" << endl;
          }
 
          int resultat_actuel = score_actuel - score_dun_coup;
 
+         // si le résultat est supérieur à 1 ou qu'on gagne par un double
          if ((resultat_actuel == 0 && coefficient == 2) || resultat_actuel > 1) 
          {
             score_de_volee_courante += score_dun_coup;
             score_actuel -= score_dun_coup;
          } 
-         else 
+         else // on bust et on remet le score de la volée courante à 0 + fléchettes à 1
          {      
             score_actuel += score_de_volee_courante;
             score_de_volee_courante = 0;
@@ -119,6 +126,7 @@ int main()
             cout << "Bust" << endl;
          }
          
+         // si on a joué les 3 fléchettes, on remet le score de volée à 0 + fléchettes à 1
          if (nombre_de_flechette_actuel > 3) 
          {
             nombre_de_flechette_actuel = 1;
@@ -127,8 +135,9 @@ int main()
       }
       coefficient = 1; // on remet les coeff. à la valeur initiale après le traitement
    } 
-   while (score_actuel != 0);
+   while (score_actuel != 0); // Tant qu'on a pas gagné
 
+   //Affichage final
    cout << "Score: " << score_actuel << " en " << nombre_flechettes_total;
    cout << " flechettes" << endl;
    cout << "Bravo!" << endl;
